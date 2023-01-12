d2c883be10234b1287565d037a5957e535afa606
Rúni H.Hansen <runi.hansen@okkara.net>
2023-01-13 00:35:17 +0100

    Set the default sql to be of type postgresql, added a databases table

--- lua/psql/init.lua
+++ lua/psql/init.lua
@@ -1,10 +1,27 @@
 local M = {}
 
+M._databases = {}
+
 M.setup = function(opts)
   -- syntax color for the .pgpass file
   vim.cmd([[
     autocmd BufNewFile,BufRead .pgpass set filetype=pgpass
   ]])
+
+  -- Set PostgreSQL to be the default sql language type
+  vim.g.sql_type_default = 'postgresql'
+
+
+  -- TODO build database tree
+  table.insert(M._databases, {
+    name = 'dbname',
+    schemas = {
+      tables = {},
+      views = {},
+      functions = {},
+      sequences = {},
+    }
+  })
 end
 
 return M
