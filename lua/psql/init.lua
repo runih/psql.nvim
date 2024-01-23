7f69596b11f80439db4ff5e1be09d730ff6459c7
Rúni H.Hansen <runi.hansen@okkara.net>
2024-01-23 22:05:53 +0100

    Remove the Colors tag from the Name

--- lua/psql/init.lua
+++ lua/psql/init.lua
@@ -1,110 +1,107 @@
 local M = {}
 
-local dbselect = require('psql.dbselect')
+local dbselect = require("psql.dbselect")
 
 M._databases = {}
 
 local option = function(value, default_value)
-  if value then
-    return value
-  end
-  return default_value
+	if value then
+		return value
+	end
+	return default_value
 end
 
 function KeyMappings()
-  -- Default mappings
-  -- Select Database
-  vim.keymap.set("n", "<leader>D", dbselect.open, { desc = "Select [D]atabase" })
+	-- Default mappings
+	-- Select Database
+	vim.keymap.set("n", "<leader>D", dbselect.open, { desc = "Select [D]atabase" })
 
-  -- Execute query
-  vim.keymap.set("i", "<C-E>", M.query, { desc = "[<ctrl>+E]xecute current query" })
-  vim.keymap.set("n", "<leader>E", M.query, { desc = "[<ctrl>]+[E]xecute current query" })
+	-- Execute query
+	vim.keymap.set("i", "<C-E>", M.query, { desc = "[<ctrl>+E]xecute current query" })
+	vim.keymap.set("n", "<leader>E", M.query, { desc = "[<ctrl>]+[E]xecute current query" })
 end
 
 M.setup = function(opts)
-  KeyMappings()
-  -- Default values
-  M.limit = option(opts.limit, 100)
-  M.offset = option(opts.offset, 0)
-  M.command = option(opts.command, 'psql')
+	KeyMappings()
+	-- Default values
+	M.limit = option(opts.limit, 100)
+	M.offset = option(opts.offset, 0)
+	M.command = option(opts.command, "psql")
 
-
-  -- syntax color for the .pgpass file
-  vim.cmd([[
+	-- syntax color for the .pgpass file
+	vim.cmd([[
     autocmd BufNewFile,BufRead .pgpass set filetype=pgpass
   ]])
 
-  -- Set PostgreSQL to be the default sql language type
-  vim.g.sql_type_default = 'postgresql'
+	-- Set PostgreSQL to be the default sql language type
+	vim.g.sql_type_default = "postgresql"
 
-
-  -- TODO build database tree
-  table.insert(M._databases, {
-    name = 'dbname',
-    schemas = {
-      tables = {},
-      views = {},
-      functions = {},
-      sequences = {},
-    }
-  })
+	-- TODO build database tree
+	table.insert(M._databases, {
+		name = "dbname",
+		schemas = {
+			tables = {},
+			views = {},
+			functions = {},
+			sequences = {},
+		},
+	})
 end
 
 local statement = function(bufnr)
-  local statement = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  return statement
+	local statement = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+	return statement
 end
 
 M.set_limit = function(limit)
-  local bufnr = vim.api.nvim_get_current_buf()
-  M.buffers[bufnr].limit = limit
+	local bufnr = vim.api.nvim_get_current_buf()
+	M.buffers[bufnr].limit = limit
 end
 
 M.set_offset = function(offset)
-  local bufnr = vim.api.nvim_get_current_buf()
-  M.buffers[bufnr].offset = offset
+	local bufnr = vim.api.nvim_get_current_buf()
+	M.buffers[bufnr].offset = offset
 end
 
 local execute = function(bufnr)
-  if not M.buffers[bufnr].output then
-    M.buffers[bufnr].output = vim.api.nvim_create_buf(true, true)
-  end
-  vim.api.nvim_buf_set_lines(M.buffers[bufnr].output, 0, 0, false, { "" })
-  for index, line in ipairs(M.buffers[bufnr].statement) do
-    vim.api.nvim_buf_set_lines(M.buffers[bufnr].output, index - 1, -1, false, {
-      line
-    })
-  end
+	if not M.buffers[bufnr].output then
+		M.buffers[bufnr].output = vim.api.nvim_create_buf(true, true)
+	end
+	vim.api.nvim_buf_set_lines(M.buffers[bufnr].output, 0, 0, false, { "" })
+	for index, line in ipairs(M.buffers[bufnr].statement) do
+		vim.api.nvim_buf_set_lines(M.buffers[bufnr].output, index - 1, -1, false, {
+			line,
+		})
+	end
 end
 
+M.query = function()
+	local bufnr = vim.api.nvim_get_current_buf()
+	if not M.buffers then
+		M.buffers = {}
+	end
+	if not M.buffers[bufnr] then
+		M.buffers[bufnr] = {
+			bufnr = bufnr,
+		}
+	end
+	if not dbselect.selected then
+		dbselect.open()
+	end
+	M.buffers[bufnr].statement = statement(bufnr)
 
-M.query = function ()
-  local bufnr = vim.api.nvim_get_current_buf()
-  if not M.buffers then
-    M.buffers = {}
-  end
-  if not M.buffers[bufnr] then
-    M.buffers[bufnr] = {
-      bufnr = bufnr,
-    }
-  end
-  if not dbselect.selected then
-    dbselect.open()
-  end
-  M.buffers[bufnr].statement = statement(bufnr)
+	if not M.buffers[bufnr].limit then
+		M.buffers[bufnr].limit = M.limit
+	end
 
-  if not M.buffers[bufnr].limit then
-    M.buffers[bufnr].limit = M.limit
-  end
-
-  if not M.buffers[bufnr].offset then
-    M.buffers[bufnr].offset = M.offset
-  end
-  if not M.buffers[bufnr].statement then
-    print("No real sql statement")
-  else
-    execute(bufnr)
-  end
+	if not M.buffers[bufnr].offset then
+		M.buffers[bufnr].offset = M.offset
+	end
+	if not M.buffers[bufnr].statement then
+		print("No real sql statement")
+	else
+		execute(bufnr)
+	end
 end
 
 -- Set commands
